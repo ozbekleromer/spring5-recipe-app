b31f7fa9656ca73627268c11d6d36e7b07097413
ozbekleromer <omerozbekler94@gmail.com>
2020-11-15 16:45:35 +0100

    Adding index page and controller

---
+++ src/main/java/com/ozbek/spring5recipeapp/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package com.ozbek.spring5recipeapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * created by ozbek on 2020-11-15
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage() {
+        return "index";
+    }
+}
